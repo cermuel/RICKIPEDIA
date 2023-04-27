186d072d5ab7d6c6153a13e26db67b53d98a09f4
Ngene Samuel Obasi <81524684+cermuel@users.noreply.github.com>
2023-04-27 15:48:31 +0100

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>
-            <div class="mt-5 p-4 bg-dark text-white text-center gap-4" style={{width: "100%"}}>
-  <div className="">
-  <a href="https://www.instagram.com/cermuel" style={{marginLeft: "10px", fontSize: "25px"}} target="_blank"><i class="fab fa-instagram"></i></a>
-  <a href="http://cermuel.000webhostapp.com/" style={{marginLeft: "10px", fontSize: "25px"}} target="_blank"><i class="fas fa-globe"></i></a>
-  <a href="https://github.com/cermuel/RickiPedia" style={{marginLeft: "10px", fontSize: "25px"}} target="_blank"><i class="fab fa-github"></i></a>
-  </div>
-  <div style={{marginTop: "10px"}}>
-  <i class="fas fa-code"></i> by <a href="http://cermuel.000webhostapp.com/" target="_blank" style={{textDecoration: "none", fontWeight: "bold"}}>Ngene Samuel</a> 2021
-  </div>
-</div>
-        </div>
+//         <div>
+//             <div class="mt-5 p-4 bg-dark text-white text-center gap-4" style={{width: "100%"}}>
+//   <div className="">
+//   <a href="https://www.instagram.com/cermuel" style={{marginLeft: "10px", fontSize: "25px"}} target="_blank"><i class="fab fa-instagram"></i></a>
+//   <a href="http://cermuel.000webhostapp.com/" style={{marginLeft: "10px", fontSize: "25px"}} target="_blank"><i class="fas fa-globe"></i></a>
+//   <a href="https://github.com/cermuel/RickiPedia" style={{marginLeft: "10px", fontSize: "25px"}} target="_blank"><i class="fab fa-github"></i></a>
+//   </div>
+//   <div style={{marginTop: "10px"}}>
+//   <i class="fas fa-code"></i> by <a href="http://cermuel.000webhostapp.com/" target="_blank" style={{textDecoration: "none", fontWeight: "bold"}}>Ngene Samuel</a> 2021
+//   </div>
+// </div>
+//         </div>
     )
 }
 
